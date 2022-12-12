8e8bb9840197d59c937791683d80e2b9176f12f5
jacobways <jacobkjy0501@gmail.com>
2022-12-12 22:49:30 +0900

    4강 프로젝트

---
+++ 4. file-based-routing-project/components/events/event-search.js
@@ -0,0 +1,15 @@
+function EventsSearch(props) {
+  return (
+    <form>
+      <div>
+        <div>
+          <label html="year">Year</label>
+          <select id="year">
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  );
+}
