11775880ddd7e4d593b17c513dd70f1deaba320f
jacob <jacobkjy0501@gmail.com>
2022-12-12 13:25:03 +0900

    pre-rendering에서 api route 사용

---
+++ 8. api-route/pages/feedback/index.js
@@ -0,0 +1,24 @@
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+// next.js는 이를 getStaticProps에서 사용되는 걸 암지하고  client bundle에 포함시키지 않음
+
+function FeedbackPage(props) {
+  return (
+    <ul>
+      {props.feedbackItems.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
