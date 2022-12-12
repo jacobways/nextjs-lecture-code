b026cf61edbbe0b8ee88ed7daf9a32e1708ec9e8
jacob <jacobkjy0501@gmail.com>
2022-12-12 17:02:16 +0900

    동적 api route

--- 8. api-route/pages/feedback/index.js
+++ 8. api-route/pages/feedback/index.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 // next.js는 이를 getStaticProps에서 사용되는 걸 암지하고  client bundle에 포함시키지 않음
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feeback);
+      });
+  }
+
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
